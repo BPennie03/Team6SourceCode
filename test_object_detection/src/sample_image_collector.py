3b97b891970bdf4b6915f2c63bc47b8b25841117
Braden Pennie <pennie.3@wright.edu>
2024-09-05 09:01:07 -0400

    Fixed image crawler

--- test_object_detection/src/sample_image_collector.py
+++ test_object_detection/src/sample_image_collector.py
@@ -1,12 +1,28 @@
-from icrawler.builtin import BingImageCrawler
+import os
+from icrawler.builtin import GoogleImageCrawler
 
 # Types of images to collect
-classes = ['death star images']
+classes = ['dog']
 
 # Number of images to collect
-number = 10
+number = 150
+
+# Path to save images
+save_path = "./test_object_detection/resources/images/samples/n"
 
 for c in classes:
-    crawler = BingImageCrawler(
-        storage={"root_dir": "test_object_detection/resources/images/samples"})
-    crawler.crawl(keyword=c, filters=None, max_num=number, offset=0)
+    downloaded_images = 0
+
+    # Continue crawling until we get the target number of images
+    while downloaded_images < number:
+        crawler = GoogleImageCrawler(
+            storage={"root_dir": save_path})
+
+        # Crawl remaining images
+        crawler.crawl(keyword=c, filters=None,
+                      max_num=number, offset=0)
+
+        # Update the number of downloaded images
+        downloaded_images = len(os.listdir(save_path))
+
+        print(f"Downloaded {downloaded_images}/{number} images")
