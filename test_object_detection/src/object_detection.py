16a9d385948ff7805469363ce361c4bfcd09f88d
Celeste Irwin <celeste.irwin@sinclair.edu>
2024-09-10 10:01:51 -0400

    WIP for updating our code to the logic we will use on the pi. Does not fully work yet

--- test_object_detection/src/object_detection.py
+++ test_object_detection/src/object_detection.py
@@ -2,8 +2,16 @@ import cv2
 import os
 from matplotlib import pyplot as plt
 
+def get_next_filename(image_name):
+    i = 0
+    while os.path.exists(image_name + "%s.png" % i):
+        i += 1
+    
+    return image_name + "%s.png" % i
 
-def detect_and_outline(image_path, classifier_path, output_path):
+
+
+def detect_and_outline(input_path, classifier_path, output_path):
     """Detects and outlines stop signs in an image.
 
     Args:
@@ -11,29 +19,40 @@ def detect_and_outline(image_path, classifier_path, output_path):
         classifier_path (string): Path to the Haar cascade classifier.
         output_path (string): Path where the output image will be saved.
     """
-    print(f'Processing image: {image_path}')
+ 
+    # assign directory
+    directory = input_path
+ 
+    # iterate over files in
+    # that directory
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith ((".png", ".jpg")):
+                print(f'Processing image: {input_path+filename}')
 
-    img = cv2.imread(image_path)
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                img = cv2.imread(os.path.join(root, filename))
+                img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    classifier = cv2.CascadeClassifier(classifier_path)
+                classifier = cv2.CascadeClassifier(classifier_path)
 
-    found = classifier.detectMultiScale(img_gray, minSize=(20, 20))
+                found = classifier.detectMultiScale(img_gray, minSize=(20, 20))
 
-    if len(found) != 0:
-        for (x, y, width, height) in found:
-            cv2.rectangle(img_rgb, (x, y), (x + height,
-                          y + width), (0, 255, 0), 5)
+                if len(found) != 0:
+                    for (x, y, width, height) in found:
+                        cv2.rectangle(img_rgb, (x, y), (x + height,
+                                        y + width), (0, 255, 0), 5)
 
-    plt.imshow(img_rgb)
-    plt.axis('off')
-    plt.savefig(output_path)
-    print(f'Output image saved to: {output_path}')
+                plt.imshow(img_rgb)
+                plt.axis('off')
+
+
+                plt.savefig(output_path + get_next_filename("output_image"))
+                print(f'Output image saved to: {output_path}')
 
 
 if __name__ == "__main__":
-    img_path = "resources/images/sign1.png"
+    img_path = "resources/images/"
     classifier_path = "resources/stop_signs.xml"
-    output_path = "output/output_image.png"  # Adjusted to match the volume mount
+    output_path = "output/"  # Adjusted to match the volume mount
     detect_and_outline(img_path, classifier_path, output_path)
