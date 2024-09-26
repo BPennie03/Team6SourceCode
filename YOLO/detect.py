1e811ce1dea52c47c8061ee0f3794e9ca05fd9c6
Braden Pennie <pennie.3@wright.edu>
2024-09-26 09:03:33 -0400

    Added filepath flag and support for single files or directories

--- YOLO/detect.py
+++ YOLO/detect.py
@@ -1,18 +1,33 @@
+import argparse
 import os
 import utils
 from ultralytics import YOLO
 
 
-def main():
-    model_path = utils.get_most_recent_version(
-        'train', 'runs/detect/')
-    model = YOLO(f'{model_path}/weights/best.pt')
+def get_model():
+    model_path = utils.get_most_recent_version('train', 'runs/detect/')
+    return YOLO(f'{model_path}/weights/best.pt')
 
-    print(f'Using {model_path}/weights/best.pt')
 
-    for file in os.listdir('test_images'):
-        model.predict(f'test_images/{file}', save=True, imgsz=640, conf=0.7)
+def detect(dir_path='test_images'):
+    model = get_model()
+    for file in os.listdir(dir_path):
+        model.predict(f'{dir_path}/{file}', save=True, imgsz=640, conf=0.7)
+
+
+def single_detect(file_path):
+    model = get_model()
+    model.predict(file_path, save=True, imgsz=640, conf=0.7)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="YOLO Detection Script")
+    parser.add_argument('-f', type=str, default='test_images',
+                        help='Path to the directory containing images for detection')
+    args = parser.parse_args()
+
+    test_input = args.f
+    if os.path.isdir(test_input):
+        detect(test_input)
+    else:
+        single_detect(test_input)
