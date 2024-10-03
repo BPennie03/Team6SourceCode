a66942f56d68d2d3745d740a9510c703a9d5f75f
Braden Pennie <pennie.3@wright.edu>
2024-10-03 15:51:58 -0400

    Updated detect.py

--- YOLO/detect.py
+++ YOLO/detect.py
@@ -1,12 +1,16 @@
 import argparse
+import shutil
+from ultralytics import YOLO
 import os
 import utils
-from ultralytics import YOLO
-import shutil
+import red_circle_detect
+
+DETECT_DIR = 'output/detect_results/'
 
 
 def get_model():
-    model_path = utils.get_most_recent_version('train', 'runs/detect/')
+    model_path = utils.get_most_recent_version('train', 'runs/detect')
+    print(f"Using model from {model_path}")
     return YOLO(f'{model_path}/weights/best.pt')
 
 
@@ -14,12 +18,13 @@ def detect(dir_path='test_images'):
     model = get_model()
     results = model.predict(dir_path, show_boxes=False, imgsz=640, conf=0.7)
     for r in results:
-        if r.boxes:
-            r.save(conf=False, boxes=False)
+        if r.boxes:  # if the result has a bounding box
+            # r.save(conf=False, boxes=False)
+            r.save()
             move_results_files()
 
 
-def move_results_files(src_dir='.', dest_dir='detect_results'):
+def move_results_files(src_dir='.', dest_dir=DETECT_DIR):
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
     for file_name in os.listdir(src_dir):
@@ -28,11 +33,18 @@ def move_results_files(src_dir='.', dest_dir='detect_results'):
                         os.path.join(dest_dir, file_name))
 
 
+def clear_output_dir(dir_path=DETECT_DIR):
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="YOLO Detection Script")
-    parser.add_argument('-f', type=str, default='test_images',
+    parser.add_argument('-f', type=str, default='resources',
                         help='Path to the directory containing images for detection')
     args = parser.parse_args()
 
     test_input = args.f
+    clear_output_dir()
     detect(test_input)
+    # red_circle_detect.process_images(DETECT_DIR) will fix this later
