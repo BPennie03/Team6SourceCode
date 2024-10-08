450f00ef9919951eef67abeeea9b98a4a78073dd
Braden Pennie <pennie.3@wright.edu>
2024-10-08 08:15:15 -0400

    Results folder now gets zipped

--- YOLO/detect.py
+++ YOLO/detect.py
@@ -48,6 +48,7 @@ def move_results_files(src_dir='.', dest_dir=DETECT_DIR):
         if file_name.startswith('results_'):
             shutil.move(os.path.join(src_dir, file_name),
                         os.path.join(dest_dir, file_name))
+    shutil.make_archive('detect_results', 'zip', dest_dir)
 
 
 def clear_output_dir(dir_path=DETECT_DIR):
