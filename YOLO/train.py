d5b45adb36baa7c9a4e4cc896bb87f7150b4aa99
Braden Pennie <pennie.3@wright.edu>
2024-11-22 16:37:59 -0500

    Updated training parameters

--- YOLO/train.py
+++ YOLO/train.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
-model.train(data='data.yaml', epochs=30, imgsz=640, device='mps')
+model.train(data='data.yaml', epochs=50, imgsz=640, device='mps')
